43e1470976106d626c41156a2c014f4f735f1bdd
Zhonyanya <andreevich10050@gmail.com>
2025-10-10 19:26:45 +0400

    Create Cursor.cs

---
+++ Cursor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    Vector2 mousePosition;
+    public Transform cameraPos;
+    public Transform playerPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = mousePosition;
+    }
+}
