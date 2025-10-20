3529581a6fe6897d028c97ad7325ec666c70802b
Zhonyanya <andreevich10050@gmail.com>
2025-10-20 20:17:52 +0400

    Update Cursor.cs

--- Cursor.cs
+++ Cursor.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//будет ещё клянусь
+//будет ещё ага
 public class Cursor : MonoBehaviour
 {
     Vector2 mousePosition;
