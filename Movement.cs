b522723f3e4af7f7e45ddbda46292f8d07d9e770
Zhonyanya <andreevich10050@gmail.com>
2025-11-29 23:02:43 +0400

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//oгоооогатдемнапотsoajfлоеаdsjghjdfhjgdfкрлывпварывррпв
+//oгоооогатдемнапотsoajfлоеаdsjghjdfhjgdfкрлывпварывррпвфа
 public class Movement : MonoBehaviour
 {
     public float speed = 5f;
